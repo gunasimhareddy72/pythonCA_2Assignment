dd633c9ad682b909ab1c89c9d282e11fd2defe45
gunasimhareddy72 <gunasimhareddy72@gmail.com>
2024-04-03 21:20:17 +0100

    imported all the libararies required to main.py python file.

---
+++ main.py
@@ -0,0 +1,12 @@
+import sqlite3
+import random
+from flask import Flask, session, render_template, request, g
+
+app = Flask(__name__, template_folder='templates')
+app.secret_key = "random random"
+
+
+
+
+
+if __name__ == '__main__':
